571e7c8ca862ae66eb97e0fd185a3faa96b36d1b
Leonardo <juhhinx@gmail.com>
2021-06-26 13:30:40 -0300

    add

---
+++ PizzaProj/src/main/java/db2/juhh/PizzaProj/App.java
@@ -0,0 +1,13 @@
+package db2.juhh.PizzaProj;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
